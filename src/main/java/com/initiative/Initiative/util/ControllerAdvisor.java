35d5e926f16e01bb8e3a7437d6c9ce1c23fefead
jdvalencia <jdvalencia@users.noreply.github.com>
2020-07-02 01:31:01 -0400

    Added Exception Handling Advices

--- src/main/java/com/initiative/Initiative/util/ControllerAdvisor.java
+++ src/main/java/com/initiative/Initiative/util/ControllerAdvisor.java
@@ -1,4 +1,30 @@
 package com.initiative.Initiative.util;
 
+import com.initiative.Initiative.DTO.ErrorResponse;
+import com.initiative.Initiative.exceptions.BadRequestException;
+import com.initiative.Initiative.exceptions.ControllerException;
+import com.initiative.Initiative.exceptions.ResourceNotFoundException;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import javax.servlet.http.HttpServletResponse;
+
+@Component
+@RestControllerAdvice
 public class ControllerAdvisor {
+
+    @ExceptionHandler
+    public ErrorResponse handleException(ControllerException e, HttpServletResponse resp) {
+        ErrorResponse err = new ErrorResponse(e);
+
+        if(e instanceof BadRequestException) {
+            resp.setStatus(400);
+        }
+        else if(e instanceof ResourceNotFoundException) {
+            resp.setStatus(404);
+        }
+
+        return err;
+    }
 }
